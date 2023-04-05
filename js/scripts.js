a1f2a08c9f44aa208d7273935d2f913800d5027a
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-05 14:06:30 -0700

    Add new function to reset current score to 0 when a 1 is rolled.
    Co-Authored-by Trent Dietzel <dietzelbiz@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,21 @@ let score = 0;
 function throwDice() {
   if (score < 30) {
     let dice = Math.floor(Math.random() * 6) + 1;
-    if (dice != 1) {
+    if (dice !== 1) {
       score = score += dice;
     } else {
       window.alert("Oops you rolled a 1... Next player!");
+      init();
     }
     } else {
     window.alert("You won!");
+    init();
   }
   return score;
+}
+
+function init() {
+  tempScore = 0;
+  score = 0;
+  window.alert(tempScore);
 }
