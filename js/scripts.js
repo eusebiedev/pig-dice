0eb43b918908fb6443b49827ba2a2a002ca59503
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-06 16:34:38 -0700

    Add function to display score count to player1 on scripts.js
    Co-Authored-by Trent Dietzel <dietzelbiz@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,50 @@
-// let score = 0;
-// function throwDice() {
-//   if (score < 30) {
-//     let dice = Math.floor(Math.random() * 6) + 1;
+// Business Logic
 
-//     score = score += dice;
-//   } else {
-//     window.alert("You won!");
-//   }
-//   return score;
-// }
-
-// let totalScore = [0, 0];
-// function throwDice() {
-//   if (totalScore < 30) {
-//     let dice = Math.floor(Math.random() * 6) + 1;
-//     if (dice !== 1) {
-//       totalScore = totalScore += dice;
-//     } else if(dice === 1) {
-//       window.alert("Oops you rolled a 1... Next player!");
-//       init();
-//       switchPlayer();
-//     }
-//     } else {
-//     window.alert("You won!");
-//     init();
-//   }
-//   return totalScore;
-// }
-
-// function init() {
-//   tempScore = 0;
-//   totalScore = 0;
-//   window.alert(tempScore);
-// }
-
-// function switchPlayer() {
-//   if (player > 0) {
-//     player = 0;
-//     console.log(player);
-//     return player;
-//   } else {
-//     player = 1;
-//     console.log(player);
-//     return player;
-//   }
-// }
-
-// function playerHold(tempScore, totalScore) {
-//   totalScore = totalScore + tempScore;
-//   switchPlayer();
-// }
-
-let totalScore = [0, 0];
-let tempScore = 0;
+let totalScore = [];
+let tempScore;
 let player;
-function throwDice() {
-  if (totalScore[0] + tempScore < 30 && totalScore[1] + tempScore < 30) {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    console.log("Current Player:" + player);
-    console.log("The dice rolled:" + dice);
-    if (dice !== 1 && player === 0) {
-      tempScore += dice;
-      console.log("Temp Score Player 0: " + tempScore);
-    } else if (dice !== 1 && player === 1) {
-      tempScore += dice;
-      console.log("Temp Score Player 1: " + tempScore);
-    } else if (dice === 1) {
-      window.alert("Oops you rolled a 1... Next player!");
-      init();
-      console.log("Temp Score: " + tempScore);
-      switchPlayer();
-      console.log("Its player" + player + "'s turn!");
-    }
-    } else {
-    window.alert("You won!");
-    init();
-  }
-  return totalScore;
-}
+
+document.querySelector('.btn-new').addEventListener('click', function() {
+  totalScore = [0, 0];
+  tempScore = 0;
+  player = 0;
+  return totalScore, tempScore, player;
+});
 
 function init() {
+  document.querySelector('.btn-new').click();
+}
+
+function reset() {
   tempScore = 0;
   return tempScore;
 }
 
-function switchPlayer() {
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  if (totalScore[0] + tempScore < 30 && totalScore[1] + tempScore < 30) {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("oops").innerHTML = " ";
+    document.getElementById("winner").innerHTML = " ";
+    if (dice !== 1 && player === 0) {
+      tempScore += dice;
+      document.getElementById("temp-score-1").innerHTML = tempScore;
+    } else if (dice !== 1 && player === 1) {
+      tempScore += dice;
+      document.getElementById("temp-score-1").innerHTML = tempScore;
+    } else if (dice === 1) {
+      document.getElementById("oops").innerHTML = "Oops you rolled a 1, Next Player";
+      reset();
+      document.getElementById("temp-score-1").innerHTML = tempScore;
+      switchPlayer();
+    }
+    } else {
+    document.getElementById("winner").innerHTML = "You Won!";
+    init();
+  }
+  return totalScore;
+});
+
+function switchPlayer()  {
   if (player > 0) {
     player = 0;
     return player;
@@ -94,12 +54,12 @@ function switchPlayer() {
   }
 }
 
-function playerHold() {
+document.querySelector('.btn-hold').addEventListener('click', function() {
   totalScore[player] += tempScore;
-  init();
-  console.log("Its player " + player + "'s turn!");
-  console.log("Total Scores: ");
-  console.log("Player 0: " + totalScore[0]);
-  console.log("Player 1: " + totalScore[1]);
+  document.getElementById("total-score-2").innerHTML = totalScore[0];
+  reset();
   return switchPlayer();
-}
+});
+// UI Logic
+
+/// WIP
